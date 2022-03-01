7e4a8502141b2f86f3ede65ae254c1a548582358
Alexandre <alexandre.nicaise.2000@gmail.com>
2022-03-01 12:27:48 +0100

    trait in stand by state for now

---
+++ classes/Connexion.trait.php
@@ -0,0 +1,18 @@
+<?php
+
+trait connexion
+{
+    private string $emailUser;
+    private string $passwordUser;
+    private string $serverName = "localhost";
+    private string $userName = "root";
+    private string $database = "poo";
+    private string $userPassword = "";
+    private object $connexion;
+
+
+    public function login()
+    {
+        $requete = 
+    }
+}
