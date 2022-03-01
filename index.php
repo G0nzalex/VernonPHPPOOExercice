3a631c6294e03a18ed8de819ed4c5580f0e7152b
Alexandre <alexandre.nicaise.2000@gmail.com>
2022-03-01 12:28:15 +0100

    .

--- index.php
+++ index.php
@@ -6,3 +6,13 @@ date_default_timezone_set('Europe/Paris');
 spl_autoload_register(function($className) {
     include './classes/' . $className . '.php';
 });
+
+$michel = new User();
+$michel->setNomUser("dupont");
+$michel->setprenomUser("dupond");
+$michel->setemail("dupont@gmail.com");
+$michel->setdateNaissance("1967-01-11");
+
+$connexion = new Sql();
+
+var_dump($michel);
